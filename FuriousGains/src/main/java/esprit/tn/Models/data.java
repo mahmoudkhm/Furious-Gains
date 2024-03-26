df87277706f6ceabb9c892754003c14030a4f3e4
nadabha1 <100917479+nadabha1@users.noreply.github.com>
2024-03-26 20:49:34 +0100

    Delete FuriousGains directory

--- FuriousGains/src/main/java/esprit/tn/Models/data.java
+++
@@ -1,8 +0,0 @@
-package esprit.tn.Models;
-
-public class data {
-    public   static  String path ;
-    public   static  Integer cID ;
-    public   static  Integer qty ;
-    public  static  Double pr ;
-}
